9f9833f08a83c21e42d325e1ff01bbe07ff6b993
enesray <iletisim.enesray@gmail.com>
2022-12-20 00:32:55 +0300

    sqlite

--- app/src/main/java/com/raytech/retrofit_ile_veri_cekmek_001/MainActivity.java
+++ app/src/main/java/com/raytech/retrofit_ile_veri_cekmek_001/MainActivity.java
@@ -3,11 +3,13 @@ package com.raytech.retrofit_ile_veri_cekmek_001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.raytech.retrofit_ile_veri_cekmek_001.model.Albums;
 import com.raytech.retrofit_ile_veri_cekmek_001.service.APIService;
+import com.raytech.retrofit_ile_veri_cekmek_001.sqlite.DatabaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,18 +25,24 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Albums> albumModels;
     private String BASE_URL = "https://jsonplaceholder.typicode.com/";
     Retrofit retrofit;
+    private DatabaseHelper dbHelper;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new DatabaseHelper(this);
+
         Gson gson = new GsonBuilder().setLenient().create();
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
-        loadData();
+
+        loadData(); //retrofit ile çekilen verilerin gösterilmesi için
+        addAlbum(); //çekilen verileri sqlite aktarmak için
     }
 
     private void loadData() {
@@ -59,6 +67,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<List<Albums>> call, Throwable t) {
                 t.printStackTrace();
+            }
+        });
+    }
+
+    private void addAlbum() {
+        APIService apiService = retrofit.create(APIService.class);
+        Call<List<Albums>> call = apiService.getAlbumList();
+
+        call.enqueue(new Callback<List<Albums>>() {
+            @Override
+            public void onResponse(Call<List<Albums>> call, Response<List<Albums>> response) {
+                if (response.isSuccessful()) {
+                    List<Albums> albums = response.body();
+                    dbHelper.addAlbums(albums);
+                    System.out.println("Veriler sqlite içerisine kaydedildi");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Albums>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Hata : " + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ app/src/main/java/com/raytech/retrofit_ile_veri_cekmek_001/sqlite/DatabaseHelper.java
@@ -0,0 +1,55 @@
+package com.raytech.retrofit_ile_veri_cekmek_001.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.raytech.retrofit_ile_veri_cekmek_001.model.Albums;
+
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "albums_db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "albums";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_USER_ID = "userId";
+    private static final String COLUMN_TITLE = "title";
+
+    private static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY,"
+                    + COLUMN_USER_ID + " INTEGER,"
+                    + COLUMN_TITLE + " TEXT"
+                    + ")";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void addAlbums(List<Albums> albums) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        for (Albums album : albums) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_ID, album.getId());
+            values.put(COLUMN_USER_ID, album.getUserId());
+            values.put(COLUMN_TITLE, album.getTitle());
+
+            db.insert(TABLE_NAME, null, values);
+        }
+        db.close();
+    }
+}
